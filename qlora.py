e738076006060084866c7bfdc58016ae27608d3c
Yuki Harada <117978472+yukiharada1228@users.noreply.github.com>
2023-08-10 17:16:04 +0900

    Update qlora.py

--- qlora.py
+++ qlora.py
@@ -347,7 +347,7 @@ def get_accelerate_model(args, checkpoint_dir):
     tokenizer = AutoTokenizer.from_pretrained(
         args.model_name_or_path,
         cache_dir=args.cache_dir,
-        padding_side="right",
+        # padding_side="right",
         use_fast=False, # Fast tokenizer giving issues.
         tokenizer_type='llama' if 'llama' in args.model_name_or_path else None, # Needed for HF name change
         trust_remote_code=args.trust_remote_code,
